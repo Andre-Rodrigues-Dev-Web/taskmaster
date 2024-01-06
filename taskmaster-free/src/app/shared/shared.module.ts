2d89ae8e30b7f88749b665e7d78e403e5a252479
André Rodrigues <79713707+Andre-Rodrigues-Dev-Web@users.noreply.github.com>
2024-01-06 13:51:04 -0300

    feat: adicionado pre estrutura

---
+++ taskmaster-free/src/app/shared/shared.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { TopoComponent } from './components/topo/topo.component';
+import { SharedServicesModule } from './services/shared-services.module';
+import { MenuComponent } from './components/menu/menu.component';
+
+
+@NgModule({
+  declarations: [
+    TopoComponent,
+    MenuComponent
+  ],
+  imports: [
+    CommonModule,
+  ],
+  providers: [
+    SharedServicesModule
+  ],
+  exports: [
+   TopoComponent
+  ],
+})
+export class SharedModule {}
---
+++ taskmaster-free/src/app/views/minhas-tarefas/minhas-tarefas.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-minhas-tarefas',
+  templateUrl: './minhas-tarefas.component.html',
+  styleUrls: ['./minhas-tarefas.component.scss']
+})
+export class MinhasTarefasComponent {
+
+}
